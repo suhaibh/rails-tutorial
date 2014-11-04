ec514526aaad1dc2fd7256ccb559d0824c59f2c2
suhaibhabibullah <shabibullah92@gmail.com>
2014-11-04 01:02:30 +0000

    fix email validation can't do x..com addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase } # converts all emails to downcase before saving
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\Z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false } # allows validation to equate user@email.com to USER@EMAIL.COM
