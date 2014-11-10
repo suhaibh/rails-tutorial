76fa60f400a5ffbf1b16ad45587739bf002b258d
suhaibhabibullah <shabibullah92@gmail.com>
2014-11-10 02:54:56 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   
   get 'signup' => 'users#new'
   
+  resources :users
+  
   get 'static_pages/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
